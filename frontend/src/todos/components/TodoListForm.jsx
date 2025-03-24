0a2d848ca33db3329352b77881b11188899f4ddd
yileicheng <yilei.cheng.cyl@gmail.com>
2025-03-24 20:00:15 +0100

    feat: use StaticDatePicker popup

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -8,14 +8,18 @@ import {
   Button,
   Typography,
   Checkbox,
+  Dialog,
+  DialogContent,
+  Divider,
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export const TodoListForm = ({ todoList, saveTodoList }) => {
   const [todos, setTodos] = useState(todoList.todos)
+  const [showDatePicker, setShowDatePicker] = useState(null)
 
   useEffect(() => {
     const autosave = () => {
@@ -40,6 +44,7 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
     const newTodos = [...todos]
     newTodos[index] = { ...newTodos[index], dueDate: date }
     setTodos(newTodos)
+    setShowDatePicker(null)
   }
 
   const handleDelete = (index) => {
@@ -60,51 +65,75 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
           <Typography component='h2'>{todoList.title}</Typography>
           <form style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
             {todos.map((todo, index) => (
-              <div
-                key={index}
-                style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}
-              >
-                <Checkbox
-                  checked={todo.completed}
-                  onChange={(event) => handleCompleteStatusChange(event, index)}
-                  size='large'
-                />
-                <Typography sx={{ margin: '8px' }} variant='h6'>
-                  {index + 1}
-                </Typography>
-                <TextField
-                  sx={{ flexGrow: 1, marginTop: '1rem', marginRight: '1rem' }}
-                  label='What to do?'
-                  value={todo.title}
-                  disabled={todo.completed}
-                  onChange={(event) => handleChange(event, index)}
-                />
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <DatePicker
-                    label='Due Date'
-                    value={todo.dueDate ? dayjs(todo.dueDate) : dayjs()}
-                    onChange={(date) => handleDateChange(date, index)}
-                    sx={{ marginTop: '3rem' }}
-                  />
-                  <Typography
-                    sx={{
-                      color: getDaysLeft(todo.dueDate) < 0 ? 'red' : 'green',
-                      marginTop: '0.5rem',
-                    }}
-                  >
-                    {getDaysLeft(todo.dueDate) < 0
-                      ? `${Math.abs(getDaysLeft(todo.dueDate))} days overdue`
-                      : `${getDaysLeft(todo.dueDate)} days left`}
-                  </Typography>
-                </div>
-                <Button
-                  sx={{ margin: '8px' }}
-                  size='small'
-                  color='secondary'
-                  onClick={() => handleDelete(index)}
+              <div key={index} style={{ marginBottom: '1rem' }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginBottom: todo.completed ? '2rem' : '1rem',
+                  }}
                 >
-                  <DeleteIcon />
-                </Button>
+                  <Checkbox
+                    checked={todo.completed}
+                    onChange={(event) => handleCompleteStatusChange(event, index)}
+                    size='large'
+                  />
+                  <Typography sx={{ margin: '8px' }} variant='h6'>
+                    {index + 1}
+                  </Typography>
+                  <TextField
+                    sx={{ flexGrow: 1, marginTop: '1rem', marginRight: '1rem' }}
+                    label='What to do?'
+                    value={todo.title}
+                    disabled={todo.completed}
+                    onChange={(event) => handleChange(event, index)}
+                  />
+                  <Button
+                    sx={{ margin: '8px' }}
+                    size='small'
+                    color='secondary'
+                    onClick={() => handleDelete(index)}
+                  >
+                    <DeleteIcon />
+                  </Button>
+                </div>
+                {!todo.completed && (
+                  <div style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+                    <Button
+                      onClick={() => setShowDatePicker(index)}
+                      sx={{
+                        marginRight: '1rem',
+                        backgroundColor: todo.dueDate ? 'inherit' : '#e6e9ed',
+                      }}
+                    >
+                      {todo.dueDate ? dayjs(todo.dueDate).format('DD/MM/YYYY') : 'Add Date'}
+                    </Button>
+                    {todo.dueDate && (
+                      <Typography
+                        variant='body2'
+                        sx={{
+                          color: getDaysLeft(todo.dueDate) < 0 ? 'red' : 'green',
+                          visibility: todo.completed ? 'hidden' : 'visible',
+                        }}
+                      >
+                        {getDaysLeft(todo.dueDate) < 0
+                          ? `${Math.abs(getDaysLeft(todo.dueDate))} days overdue`
+                          : `${getDaysLeft(todo.dueDate)} days left`}
+                      </Typography>
+                    )}
+                  </div>
+                )}
+                <Dialog open={showDatePicker === index}>
+                  <DialogContent>
+                    <StaticDatePicker
+                      orientation='landscape'
+                      openTo='day'
+                      value={dayjs(todo.dueDate)}
+                      onAccept={(date) => handleDateChange(date, index)}
+                    />
+                  </DialogContent>
+                </Dialog>
+                <Divider />
               </div>
             ))}
             <CardActions>
@@ -112,7 +141,7 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
                 type='button'
                 color='primary'
                 onClick={() => {
-                  setTodos([...todos, { title: '', completed: false, dueDate: dayjs() }])
+                  setTodos([...todos, { title: '', completed: false, dueDate: null }])
                 }}
               >
                 Add Todo <AddIcon />
