0a1e2f3a26642cf5824c696a5126c6fae5dc4401
yileicheng <yilei.cheng.cyl@gmail.com>
2025-03-26 17:41:45 +0100

    chore: adjust file hierarchy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import todoListRoutes from '../routes/todoListRoutes.js'
+import todoListRoutes from './routes/todoListRoutes.js'
 
 const app = express()
 const PORT = 3001
